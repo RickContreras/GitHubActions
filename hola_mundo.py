f0739919d27e7691fcc274a1c385bd6a6bc5af57
Ricardo Contreras <102176435+RickContreras@users.noreply.github.com>
2024-06-01 13:14:05 -0500

    Update hola_mundo.py
    Prueba sin variables de entorno.

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,15 +1,16 @@
 import os
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
 def main():
     # Cargar las variables de entorno desde el archivo .env
-    load_dotenv()
+    #load_dotenv()
     
     # Obtener el nombre de usuario desde la variable de entorno USERNAME
-    nombre = os.getenv("USERNAME")
+    # $nombre = os.getenv("USERNAME")
     
     # Imprimir el saludo personalizado
-    print(f"¡Hola, {nombre} desde GitHub!")
+    # print(f"¡Hola, {nombre} desde GitHub!")
+    print(f"¡Hola desde GitHub!")
 
 if __name__ == "__main__":
     main()
